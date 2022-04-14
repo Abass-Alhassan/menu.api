7e034098732907fa2b1bec27da651d733ada30d5
Abass-Alhassan <icewrldspacely@gmail.com>
2022-04-14 07:34:15 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const morgan =require("morgan")
+require("dotenv").config()
+const connectDB = require("./config/connectDB")
+const foodRoute = require("./routes/foodRoute")
+
+const app= express()
+connectDB()
+
+
+
+//middleware
+app.use(express.json())
+app.use(morgan("dev"))
+app.use(foodRoute)
+const PORT = process.env.PORT||9000
+//home route
+app.get("/", (req, res)=>{
+    res.json("Welcome to My Menu API")
+})
+app.listen(PORT,(req, res)=>{
+    console.log("server is up!!!")
+})
